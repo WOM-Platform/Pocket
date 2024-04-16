9a5c72330f1ffd93478bedb9e8b4ba48323f661e
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2024-04-16 11:26:28 +0200

    add missing files

---
+++ lib/src/scanner/application/scanner_state.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'scanner_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$scannerNotifierHash() => r'6985cab5337a0760a8324fcf3a0d5bf71457ab9e';
+
+/// See also [ScannerNotifier].
+@ProviderFor(ScannerNotifier)
+final scannerNotifierProvider =
+    AutoDisposeNotifierProvider<ScannerNotifier, ScannerState>.internal(
+  ScannerNotifier.new,
+  name: r'scannerNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$scannerNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ScannerNotifier = AutoDisposeNotifier<ScannerState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
